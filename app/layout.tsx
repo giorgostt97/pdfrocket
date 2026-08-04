64f47dc337285a8121c6a8df733f6806b022724c
giorgostt97 <klevoitems@gmail.com>
2026-08-04 13:58:25 +0300

    Add Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "react-hot-toast";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const geist = Geist({
   subsets: ["latin"],
@@ -111,6 +112,17 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className="bg-black text-white min-h-screen">
+
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xx2rt73yd4");
+          `}
+        </Script>
+
         <Toaster
           position="top-right"
           toastOptions={{
